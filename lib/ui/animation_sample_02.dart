3d836984b231917eeec757da6371738e7f730525
naoki-ishii <kyoumi155@gmail.com>
2019-12-22 17:55:59 +0900

    Split path morphing animation to individual animations.

--- lib/ui/animation_sample_02.dart
+++ lib/ui/animation_sample_02.dart
@@ -13,23 +13,53 @@ class AnimationSample02Page extends StatefulWidget {
 }
 
 class _AnimationSample02PageState extends State<AnimationSample02Page> {
+  final List<String> _animations = [
+    '4_to_5',
+    '5_to_6',
+    '6_to_7',
+    '7_to_8',
+    '8_to_3',
+    '3_to_4',
+  ];
+
+  int _animationIndex;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Animation Sample 02'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(24.0),
-        child: Container(
-          width: 300.0,
-          height: 300.0,
-          child: FlareActor(
-            'images/path_morph_sample.flr',
-            animation: 'polygon_morph',
-            fit: BoxFit.contain,
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Container(
+              width: 300.0,
+              height: 300.0,
+              child: FlareActor(
+                'images/path_morph.flr',
+                fit: BoxFit.contain,
+                animation: _animationIndex != null
+                    ? _animations[_animationIndex]
+                    : null,
+              ),
+            ),
           ),
-        ),
+          FloatingActionButton(
+            child: Icon(Icons.autorenew),
+            onPressed: () {
+              setState(() {
+                if (_animationIndex == null ||
+                    _animationIndex == _animations.length - 1) {
+                  _animationIndex = 0;
+                } else {
+                  _animationIndex++;
+                }
+              });
+            },
+          )
+        ],
       ),
     );
   }
